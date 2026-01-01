7b46eec77152c8350b74a943dcf93fb2447cdbbf
demo-project-angular <kaniket110@gmail.com>
2026-01-01 22:14:32 +0530

    add header component and update route

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainLayoutComponent } from './layouts/mainLayoutsComponents';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainLayoutComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('my-angular-app');
+  protected readonly title = signal('DemoProject');
 }
