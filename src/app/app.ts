bed27548c9922d5e0f55df58162951a305a84404
aniket <kaniket110@gmail.com>
2026-01-01 22:24:53 +0530

    Revert "add header component and update route"
    This reverts commit 7b46eec77152c8350b74a943dcf93fb2447cdbbf.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MainLayoutComponent } from './layouts/mainLayoutsComponents';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainLayoutComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('DemoProject');
+  protected readonly title = signal('my-angular-app');
 }
